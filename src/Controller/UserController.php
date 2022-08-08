e160be690e5f324b5c08ddf4cd132ab1ed874233
Dylan Germann <dev.dyger@gmail.com>
2022-08-08 17:50:12 +0200

    feat: uses function and serializer i UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,52 +2,74 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Service\UserService;
+use App\Service\ValidatorService;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class UserController extends AbstractController
 {
 
 
-    public function __construct(private readonly UserService $userService)
+    public function __construct(private readonly UserService $userService, private readonly  SerializerInterface $serializer)
     {
     }
 
     #[Route('/api/users', name: 'users', methods: ['GET'])]
     public function getAllUser(): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        try {
+            $users = $this->userService->getAllUser();
+            $jsonUsers = $this->serializer->serialize($users, 'json', ["groups" => "userList"]);
+            return new JsonResponse($jsonUsers, Response::HTTP_OK, [], true);
+        } catch (Exception $exception) {
+            return new JsonResponse($exception->getMessage(), $exception->getCode(), []);
+        }
     }
 
     #[Route('/api/users/{id}', name: 'detail_user', methods: ['GET'])]
     public function getDetailUser(int $id): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller detail!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        try {
+            $user = $this->userService->getUserDetail($id);
+            $jsonUser = $this->serializer->serialize($user, 'json', ["groups" => "userDetails"]);
+            return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
+        } catch (Exception $exception) {
+            return new JsonResponse($exception->getMessage(), $exception->getCode(), []);
+        }
     }
 
     #[Route('/api/users', name: 'create_user', methods: ['POST'])]
-    public function createUser(): JsonResponse
+    public function createUser(Request $request): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller creation!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+       try {
+           /** @var User $manager */
+           $manager = $this->getUser();
+           if (null !== $manager) {
+               $userInformation = json_decode($request->getContent(), true);
+               ValidatorService::validateCreateUserArray($userInformation);
+               $this->userService->createUser($userInformation, $manager);
+               return new JsonResponse("User correctly added", Response::HTTP_OK, []);
+           }
+       } catch (Exception $exception) {
+           return new JsonResponse($exception->getMessage(), $exception->getCode(), []);
+       }
     }
 
     #[Route('/api/users/{id}', name: 'delete_user', methods: ['DELETE'])]
     public function deleteUser(int $id): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller delete!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        try {
+           $this->userService->deleteUser($id);
+            return new JsonResponse("User correctly deleted", Response::HTTP_OK, []);
+        } catch (Exception $exception) {
+            return new JsonResponse($exception->getMessage(), $exception->getCode(), []);
+        }
     }
 }
