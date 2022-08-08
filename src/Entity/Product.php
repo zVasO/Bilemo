4ce2801e43c517722f86f1d8dd8039c35d9b2403
Dylan Germann <dev.dyger@gmail.com>
2022-08-08 17:15:13 +0200

    feat: use the functions and serializer in ProductController

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ProductRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
 class Product
@@ -12,30 +13,39 @@ class Product
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(["productList"])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(["productList", "productDetails"])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(["productList", "productDetails"])]
     private ?string $brand = null;
 
     #[ORM\Column]
+    #[Groups(["productList", "productDetails"])]
     private ?float $screenSize = null;
 
     #[ORM\Column]
+    #[Groups(["productList", "productDetails"])]
     private ?int $ram = null;
 
     #[ORM\Column]
+    #[Groups(["productList", "productDetails"])]
     private ?int $storage = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(["productList", "productDetails"])]
     private ?string $color = null;
 
     #[ORM\Column]
+    #[Groups(["productList"])]
     private ?float $price = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(["productList"])]
     private ?string $description = null;
 
     public function getId(): ?int
