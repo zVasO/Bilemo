fedf83554e37b14f25db05c7d5fc55a2530fee43
Dylan Germann <dev.dyger@gmail.com>
2022-08-15 14:52:22 +0200

    fix: remove all useless comment, fix erros from Symfonyinsight

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -55,29 +55,4 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
 
         $this->add($user, true);
     }
-
-//    /**
-//     * @return User2[] Returns an array of User2 objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?User2
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
